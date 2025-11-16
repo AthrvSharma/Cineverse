bf51fa72e706c3667ea5396aee09ed32201b1102
Athrv Sharma <athrvsharma21@gmail.com>
2025-11-16 11:28:17 +0530

    Add provided credentials configuration

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
-  MongoURI: process.env.MONGO_URI || '',
-  GoogleClientID: process.env.GOOGLE_CLIENT_ID || '',
-  GoogleClientSecret: process.env.GOOGLE_CLIENT_SECRET || ''
+  MongoURI: 'mongodb+srv://cineverse_admin:testing1234@cluster0.ehefz92.mongodb.net/cineverse?retryWrites=true&w=majority&appName=Cluster0',
+  GoogleClientID: '840040690568-b3gqt6vtiqnipkgq4g698j1j3kbap6tt.apps.googleusercontent.com',
+  GoogleClientSecret: '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'
 };
